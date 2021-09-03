f32818f16c2baac6db46d33b6abfb1f6d706a9eb
Brice Friha <Brice.friha@outlook.com>
2021-09-03 22:10:12 +0100

    GTK

--- Program.ts
+++ Program.ts
@@ -1,3 +1,25 @@
-let msg : string = "Hello World!!";
 
-console.log(msg);
+import Gtk from 'gnome-gtk';
+
+//Gtk.init(null);
+
+class MyWindow extends Gtk.Window {
+  button: Gtk.Button;
+
+  constructor() {
+    super(Gtk.WindowType.GTK_WINDOW_TOPLEVEL);
+    this.button = new Gtk.Button();
+    this.button.label = 'Click here';
+    this.button.connect('clicked', this.handleClicked);
+    this.add(this.button);
+  }
+
+  handleClicked() {
+    //console.log('Hello World');
+  }
+}
+
+const win = new MyWindow();
+win.connect('delete-event', () => Gtk.main_quit());
+win.show_all();
+Gtk.main();
